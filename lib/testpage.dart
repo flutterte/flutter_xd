5153b5d5dc36da7d2447c4f8919807892c05d582
lozn <91799281+lozn@users.noreply.github.com>
2021-12-20 14:30:47 +0800

    flutter xd

--- lib/testpage.dart
+++ lib/testpage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:preview/preview.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_xd1/util/ColorsUtil.dart';
+// import 'package:preview/preview.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,10 +12,32 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title:
+              Text('Material App Bar', style: TextStyle(color: Colors.white)),
         ),
         body: Column(
           children: [
+            Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                  child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), label: Text('收货单号')),
+                      obscureText: false,
+                      style:
+                          TextStyle(color: ColorsUtil.hexToColor("#ff0000ff"))),
+                )),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), label: Text('收货单号')),
+                  obscureText: false,
+                  style: TextStyle(color: ColorsUtil.hexToColor("#ff0000ff"))),
+            ),
+            Container(
+              child: Text('Hello World'),
+            ),
             Container(
               child: Text('Hello World'),
             )
